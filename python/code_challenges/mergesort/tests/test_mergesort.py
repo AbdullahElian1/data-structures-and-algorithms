344805026e614c94113351ad82b0620b31d3c7af
AbdullahELian1 <abodelian28@gmail.com>
2021-08-30 14:47:12 +0300

    add test for mergesort

--- python/code_challenges/mergesort/tests/test_mergesort.py
+++ python/code_challenges/mergesort/tests/test_mergesort.py
@@ -1,5 +1,20 @@
+from mergesort.merg import *
 from mergesort import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+
+
+
+def test_check_sortion():
+
+    arr=[8,4,23,42,16,15]
+    mergesort(arr)
+
+    actual=arr
+    expected=[4, 8, 15, 16, 23, 42]
+
+    assert  actual==expected

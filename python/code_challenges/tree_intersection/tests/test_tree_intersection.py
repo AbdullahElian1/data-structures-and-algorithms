48eb1f6794670d3cc2b2994fd86f55f7687afd0c
AbdullahELian1 <abodelian28@gmail.com>
2021-09-04 13:09:12 +0300

    add unit test for tree intersection function

--- python/code_challenges/tree_intersection/tests/test_tree_intersection.py
+++ python/code_challenges/tree_intersection/tests/test_tree_intersection.py
@@ -1,5 +1,33 @@
 from tree_intersection import __version__
-
+from tree_intersection.hash_table import *
+from tree_intersection.tree import *
+from tree_intersection.tree_intersection import *
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+
+def test_tree_intersection():
+    tree1=BinaryTree()
+    root1=Node(100)
+    root1.left=Node(200)
+    root1.left.left=Node(300)
+    root1.left.right=Node(400)
+    root1.right=Node(500)
+    root1.right.left=Node(600)
+    tree1.root=root1
+
+    tree2=BinaryTree()
+    root=Node(100)
+    root.left=Node(500)
+    root.left.left=Node(1000)
+    root.left.right=Node(600)
+    root.right=Node(687)
+    root.right.left=Node(7689)
+    root.right.left.left=Node(300)
+
+    tree2.root=root
+
+    assert hash_map_tree_intersection(tree1,tree2)==[500,600,100,300]
+

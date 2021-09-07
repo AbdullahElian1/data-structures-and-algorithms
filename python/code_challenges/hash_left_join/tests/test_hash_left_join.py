2f4a1d7a33edc273595ff1e7df36390166de3e84
AbdullahELian1 <abodelian28@gmail.com>
2021-09-07 16:27:30 +0300

    add second unittest

--- python/code_challenges/hash_left_join/tests/test_hash_left_join.py
+++ python/code_challenges/hash_left_join/tests/test_hash_left_join.py
@@ -30,3 +30,19 @@ def test_left_join():
     assert hashmap_left_join(hash1, hash2) == [['outfit', 'garb', 'Null'], ['guide', 'usher', 'follow'], ['wrath', 'anger', 'delight'], ['diligent', 'employed', 'idle'], ['fond', 'enamored', 'averse']]
 
 
+def test_left_join_no_matching():
+    hash1 = HashTable()
+    hash1.add('pond', 'enamored')
+    hash1.add('rath', 'anger')
+    hash1.add('adiligent', 'employed')
+    hash1.add('poutfit', 'garb')
+    hash1.add('hangguide', 'usher')
+
+    hash2 = HashTable()
+    hash2.add('fond', 'averse')
+    hash2.add('wrath', 'delight')
+    hash2.add('diligent', 'idle')
+    hash2.add('guide', 'follow')
+    hash2.add('flow', 'jam')
+
+    assert hashmap_left_join(hash1, hash2) == [['poutfit', 'garb', 'Null'], ['hangguide', 'usher', 'Null'], ['adiligent', 'employed', 'Null'], ['rath', 'anger', 'Null'], ['pond', 'enamored', 'Null']]
